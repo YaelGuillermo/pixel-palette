c1e2734eb829a6eeb94a8c1a95ea2a45b5b449b3
YaelGuillermo <l20211785@tectijuana.edu.mx>
2024-04-19 23:45:09 -0700

    zoom manage

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import sys
 import os
 import pyautogui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget, QHBoxLayout, QMessageBox
-from PyQt5.QtGui import QPixmap, QColor, QClipboard, QImage
+from PyQt5.QtGui import QPixmap, QClipboard
 from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QPixmap
 
 SCREENSHOT_IMAGE = 'screenshot.png'
 SCREENSHOT_WIDTH = 800
 SCREENSHOT_HEIGHT = 400
+ZOOM_NORMAL = 1.0
+ZOOM_FACTOR = 0.05
 
 def clear_screen():
     os.system('clear')
@@ -16,8 +19,9 @@ class MainWindow(QMainWindow):
     clear_screen()
     def __init__(self):
         super().__init__()
+
         self.screenshot = None
-        self.zoom_factor = 1.0
+        self.zoom_factor = ZOOM_NORMAL
         self.setWindowTitle("Pixel Palette")
 
         style_sheet = open('styles.css').read()
@@ -29,7 +33,23 @@ class MainWindow(QMainWindow):
 
         self.button = QPushButton("Take Screenshot", self)
         self.button.clicked.connect(self.take_screenshot)
-        layout.addWidget(self.button, alignment=Qt.AlignHCenter)
+
+        self.zoom_in_button = QPushButton("+", self)
+        self.zoom_in_button.clicked.connect(self.zoom_in)
+
+        self.zoom_out_button = QPushButton("-", self)
+        self.zoom_out_button.clicked.connect(self.zoom_out)
+
+        self.zoom_reset_button = QPushButton("Reset", self)
+        self.zoom_reset_button.clicked.connect(self.zoom_reset)
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.button)
+        button_layout.addWidget(self.zoom_in_button)
+        button_layout.addWidget(self.zoom_out_button)
+        button_layout.addWidget(self.zoom_reset_button)
+        button_layout.addStretch()  # Add stretch to push buttons to the right
+        layout.addLayout(button_layout)
 
         self.image_label = QLabel(self)
         self.image_label.setAlignment(Qt.AlignCenter)
@@ -56,20 +76,8 @@ class MainWindow(QMainWindow):
 
         self.copy_hexa_button = QPushButton("Copy HEXA", self)
         self.copy_hexa_button.clicked.connect(self.copy_hexa)
-        self.copy_hexa_button.setVisible(False) 
-        '''
-        self.zoom_in_button = QPushButton("+ Zoom", self)
-        self.zoom_in_button.clicked.connect(self.zoom_in)
-        layout.addWidget(self.zoom_in_button)
+        self.copy_hexa_button.setVisible(False)
 
-        self.zoom_out_button = QPushButton("- Zoom", self)
-        self.zoom_out_button.clicked.connect(self.zoom_out)
-        layout.addWidget(self.zoom_out_button)
-
-        self.reset_zoom_button = QPushButton("Reset Zoom", self)
-        self.reset_zoom_button.clicked.connect(self.reset_zoom)
-        layout.addWidget(self.reset_zoom_button)
-        '''
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.copy_rgb_button)
         button_layout.addWidget(self.copy_hexa_button)
@@ -101,16 +109,19 @@ class MainWindow(QMainWindow):
     def get_pixel_color(self, event):
         x = event.pos().x()
         y = event.pos().y()
-        color = self.screenshot.getpixel((x, y))
-        self.copy_rgb_button.setVisible(True) 
-        self.copy_hexa_button.setVisible(True)
-        self.color_label.setVisible(True)
-        self.hexa_label.setVisible(True)
-        self.color_box.setVisible(True)    
-        self.color_label.setText("RGB: ({}, {}, {})".format(color[0], color[1], color[2]))
-        hex_color = '#{0:02x}{1:02x}{2:02x}'.format(color[0], color[1], color[2])
-        self.hexa_label.setText("HEXA: {}".format(hex_color))
-        self.color_box.setStyleSheet("background-color: rgb({},{},{}); border: 0.5px solid #0F0F0F; border-radius: 5px;".format(color[0], color[1], color[2]))
+        if 0 <= x < SCREENSHOT_WIDTH and 0 <= y < SCREENSHOT_HEIGHT:
+            color = self.screenshot.getpixel((x, y))
+            self.copy_rgb_button.setVisible(True) 
+            self.copy_hexa_button.setVisible(True)
+            self.color_label.setVisible(True)
+            self.hexa_label.setVisible(True)
+            self.color_box.setVisible(True)    
+            self.color_label.setText("RGB: ({}, {}, {})".format(color[0], color[1], color[2]))
+            hex_color = '#{0:02x}{1:02x}{2:02x}'.format(color[0], color[1], color[2])
+            self.hexa_label.setText("HEXA: {}".format(hex_color))
+            self.color_box.setStyleSheet("background-color: rgb({},{},{}); border: 0.5px solid #0F0F0F; border-radius: 5px;".format(color[0], color[1], color[2]))
+        else:
+            return
 
     def copy_rgb(self):
         clipboard = QApplication.clipboard()
@@ -119,27 +130,31 @@ class MainWindow(QMainWindow):
     def copy_hexa(self):
         clipboard = QApplication.clipboard()
         clipboard.setText(self.hexa_label.text())
-    
+
     def zoom_in(self):
-        self.zoom_factor += 0.1  # Aumentar el factor de escala
-        self.update_image()
+        self.zoom_factor += ZOOM_FACTOR
+        self.apply_zoom()
 
     def zoom_out(self):
-        self.zoom_factor -= 0.1  # Reducir el factor de escala
-        self.update_image()
-    
-    def reset_zoom(self):
-        self.zoom_factor = 1.0  # Restablecer el factor de escala
-        self.update_image()
-    
-    def update_image(self):
-        if self.screenshot is not None:
-            scaled_pixmap = self.screenshot.scaled(SCREENSHOT_WIDTH * self.zoom_factor, SCREENSHOT_HEIGHT * self.zoom_factor)
-            self.image_label.setPixmap(scaled_pixmap)
+        self.zoom_factor -= ZOOM_FACTOR
+        self.apply_zoom()
 
+    def zoom_reset(self):
+        self.zoom_factor = ZOOM_NORMAL
+        self.apply_zoom()
+
+    def apply_zoom(self):
+        new_width = int(SCREENSHOT_WIDTH / self.zoom_factor)
+        new_height = int(SCREENSHOT_HEIGHT / self.zoom_factor)
+        self.screenshot = self.screenshot.resize((new_width, new_height))
+        self.screenshot.save(SCREENSHOT_IMAGE)
+        pixmap = QPixmap(SCREENSHOT_IMAGE)
+        self.image_label.setPixmap(pixmap)
+
+
+            
+    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec_())
-
-
